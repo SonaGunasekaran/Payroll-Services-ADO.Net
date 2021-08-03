e6b7f700a16687877591db440c5d1c8093f6130e
[Sona] <[sona16061999@gmail.com]>
2021-08-04 00:06:52 +0530

    [Sona] Creating connecting with Database

---
+++ EmployeePayroll/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace EmployeePayroll
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=employee_payroll";
+        //creating sql connection 
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
