b11a865e22d3a1a090bd85b8db941442045e3c59
[Sona] <[sona16061999@gmail.com]>
2021-08-04 20:10:34 +0530

    [Sona] Update the salary And sync with database

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -7,9 +7,24 @@ namespace EmployeePayroll
         static void Main(string[] args)
         {
             Console.WriteLine("Payroll service database!");
-                EmployeeRepo emp = new EmployeeRepo();
-                emp.GetAllData();
+            EmployeeRepo emp = new EmployeeRepo();
+            EmployeeData data = new EmployeeData();
+            Console.WriteLine("1.Retrive all data\n2.Update salary\n3.Exit");
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    emp.GetAllData();
+                    break;
+                case 2:
+                    emp.UpdateSalary(data);
+                    emp.GetAllData();
+                    break;
+                default:
+                    break;
 
+            }
         }
     }
 }
