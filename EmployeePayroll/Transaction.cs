d639aa2cde7e92c493453e21b5f8311bd3053dab
[Sona] <[sona16061999@gmail.com]>
2021-08-09 17:12:18 +0530

    [Sona] Insert into DB without thread

--- EmployeePayroll/Transaction.cs
+++ EmployeePayroll/Transaction.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq;
 using System.Data.SqlClient;
 
 namespace EmployeePayroll
@@ -42,7 +45,7 @@ namespace EmployeePayroll
         }
         public int CascadeDelete()
         {
-            int count= 0;
+            int count = 0;
             using (sqlConnection)
             {
                 sqlConnection.Open();
@@ -65,5 +68,92 @@ namespace EmployeePayroll
             return count;
         }
 
+        public string AddIsActiveColumn()
+        {
+            string result = null;
+            using (sqlConnection)
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+                try
+                {
+                    sqlCommand.CommandText = "Alter table Employee add IsActive int NOT NULL default 1";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                    result = "Active Column Added";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    sqlTransaction.Rollback();
+                    result = "Column not Updated";
+                }
+            }
+            sqlConnection.Close();
+            return result;
+        }
+        public List<EmployeeData> RetriveDataForAudit(string name)
+        {
+            using (sqlConnection)
+            {
+                sqlConnection.Open();
+                EmployeeData employee = new EmployeeData();
+                List<EmployeeData> employeeList = new List<EmployeeData>();
+                try
+                {
+                    SqlCommand sqlCommand = new SqlCommand(name, sqlConnection);
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            employee.EmployeeId = Convert.ToInt32(reader["Employee_Id"] == DBNull.Value ? default : reader["Employee_Id"]);
+                            employee.EmployeeName = reader["Employee_name"] == DBNull.Value ? default : reader["Employee_name"].ToString();
+                            employee.Gender = reader["Gender"] == DBNull.Value ? default : reader["Gender"].ToString();
+                            employee.StartDate = reader["StartDate"] == DBNull.Value ? default : reader["StartDate"].ToString(); ;
+                            employee.PhoneNumber = Convert.ToDouble(reader["PhoneNumber"] == DBNull.Value ? default : reader["PhoneNumber"]);
+                            employee.Address = reader["Address"] == DBNull.Value ? default : reader["Address"].ToString();
+                            employee.NetPay = Convert.ToDouble(reader["NetPay"] == DBNull.Value ? default : reader["NetPay"]);
+                            employee.Department = reader["Department_Name"] == DBNull.Value ? default : reader["Department_Name"].ToString();
+                            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} ", employee.EmployeeId, employee.EmployeeName, employee.Gender, employee.StartDate, employee.PhoneNumber, employee.Address, employee.NetPay);
+                            employeeList.Add(employee);
+                        }
+                        reader.Close();
+                        return employeeList;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return employeeList;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+        public long InsertWithoutThread()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            AddData();
+            stopwatch.Stop();
+            return (stopwatch.ElapsedMilliseconds);
+
+        }
+        void AddData()
+        {
+            AddingRecord(new EmployeeData { EmployeeName = "Guna", CompanyId = 2, Address = "Zora Palace", City = "Madurai", State = "TamilNadu", StartDate = "2021-07-19", Gender = "M", PhoneNumber = 1234346547, DepartmentId = 3, BasicPay = 65000 });
+            AddingRecord(new EmployeeData { EmployeeName = "Adit", CompanyId = 2, Address = "Incara", City = "Cbe", State = "Kerala", StartDate = "2021-07-18", Gender = "M", PhoneNumber = 8877664422, DepartmentId = 2, BasicPay = 6000 });
+        } 
     }
+
 }
